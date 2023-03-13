23b2d4f53d9c7d6779dc9e49ed40d2c5c5bf77ef
bayupamuji <bayuteguhpamuji@gmail.com>
2023-03-13 16:15:07 +0700

    create simple ui calc

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CalculatorApp()
                 }
             }
         }
@@ -30,14 +33,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CalculatorApp(modifier: Modifier = Modifier) {
+    var lengthInput by remember { mutableStateOf("") }
+    var widthInput by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+
+    val length = lengthInput.toDoubleOrNull() ?: 0.0
+    val width = widthInput.toDoubleOrNull() ?: 0.0
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), contentAlignment = Alignment.Center
+    ) {
+        Column {
+            TextField(
+                value = lengthInput,
+                onValueChange = { lengthInput = it },
+                label = {
+                    Text(
+                        text = stringResource(
+                            id = R.string.enter_length
+                        )
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = modifier.height(8.dp))
+            TextField(
+                value = widthInput,
+                onValueChange = { widthInput = it },
+                label = {
+                    Text(
+                        text = stringResource(
+                            id = R.string.enter_width
+                        )
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Button(onClick = { result = (length * width).toString() }) {
+                Text(stringResource(id = R.string.count))
+            }
+            Text(text = stringResource(id = R.string.result, result))
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CalculatorAppTheme {
-        Greeting("Android")
+        CalculatorApp()
     }
 }
